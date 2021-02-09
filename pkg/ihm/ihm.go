3256276e556e6c59ef53a044c83d9e0fef05f9ca
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2021-02-09 16:48:19 +0100

    test

--- pkg/ihm/ihm.go
+++ pkg/ihm/ihm.go
@@ -1,11 +1,7 @@
 package ihm
 
 import (
-	"fmt"
 	"log"
-	"math"
-	"math/rand"
-	"time"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
@@ -20,54 +16,11 @@ func App() {
 	}
 	defer ui.Close()
 
-	rand.Seed(time.Now().UTC().UnixNano())
-	randomDataAndOffset := func() (data []float64, offset float64) {
-		noSlices := 1 + rand.Intn(5)
-		data = make([]float64, noSlices)
-		for i := range data {
-			data[i] = rand.Float64()
-		}
-		offset = 2.0 * math.Pi * rand.Float64()
-		return
-	}
-
-	pc := widgets.NewPieChart()
-	pc.Title = "Pie Chart"
-	pc.SetRect(5, 5, 70, 36)
-	pc.Data = []float64{.25, .25, .25, .25}
-	pc.AngleOffset = -.5 * math.Pi
-	pc.LabelFormatter = func(i int, v float64) string {
-		return fmt.Sprintf("%.02f", v)
-	}
-
-	pause := func() {
-		run = !run
-		if run {
-			pc.Title = "Pie Chart"
-		} else {
-			pc.Title = "Pie Chart (Stopped)"
-		}
-		ui.Render(pc)
-	}
-
-	ui.Render(pc)
-
-	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Second).C
-	for {
-		select {
-		case e := <-uiEvents:
-			switch e.ID {
-			case "q", "<C-c>":
-				return
-			case "s":
-				pause()
-			}
-		case <-ticker:
-			if run {
-				pc.Data, pc.AngleOffset = randomDataAndOffset()
-				ui.Render(pc)
-			}
-		}
-	}
+	g0 := widgets.NewGauge()
+	g0.Title = "Slim Gauge"
+	g0.SetRect(20, 20, 30, 30)
+	g0.Percent = 75
+	g0.BarColor = ui.ColorRed
+	g0.BorderStyle.Fg = ui.ColorWhite
+	g0.TitleStyle.Fg = ui.ColorCyan
 }
