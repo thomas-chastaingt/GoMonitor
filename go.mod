0c950ee2c215cd6bc90bdb53fa5e79a4e7a5c1bd
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2021-02-09 16:44:33 +0100

    test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/gizak/termui/v3 v3.1.0
 	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/shirou/gopsutil v3.21.1+incompatible
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
--- pkg/ihm/ihm.go
+++ pkg/ihm/ihm.go
@@ -1,27 +1,73 @@
 package ihm
 
 import (
+	"fmt"
 	"log"
+	"math"
+	"math/rand"
+	"time"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 )
 
+const run = true
+
 func App() {
+	//init the MHI
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize user interface %v", err)
 	}
 	defer ui.Close()
-	p := widgets.NewParagraph()
-	p.Text = "Hello World!"
-	p.SetRect(0, 0, 25, 5)
 
-	ui.Render(p)
-
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
+	rand.Seed(time.Now().UTC().UnixNano())
+	randomDataAndOffset := func() (data []float64, offset float64) {
+		noSlices := 1 + rand.Intn(5)
+		data = make([]float64, noSlices)
+		for i := range data {
+			data[i] = rand.Float64()
 		}
+		offset = 2.0 * math.Pi * rand.Float64()
+		return
 	}
 
+	pc := widgets.NewPieChart()
+	pc.Title = "Pie Chart"
+	pc.SetRect(5, 5, 70, 36)
+	pc.Data = []float64{.25, .25, .25, .25}
+	pc.AngleOffset = -.5 * math.Pi
+	pc.LabelFormatter = func(i int, v float64) string {
+		return fmt.Sprintf("%.02f", v)
+	}
+
+	pause := func() {
+		run = !run
+		if run {
+			pc.Title = "Pie Chart"
+		} else {
+			pc.Title = "Pie Chart (Stopped)"
+		}
+		ui.Render(pc)
+	}
+
+	ui.Render(pc)
+
+	uiEvents := ui.PollEvents()
+	ticker := time.NewTicker(time.Second).C
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "s":
+				pause()
+			}
+		case <-ticker:
+			if run {
+				pc.Data, pc.AngleOffset = randomDataAndOffset()
+				ui.Render(pc)
+			}
+		}
+	}
 }
