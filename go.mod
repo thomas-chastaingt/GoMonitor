3194c4f0e70c5c2ff91865e08a02fc474f36eab8
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2021-02-09 15:11:38 +0100

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/thomas-chastaingt/GoMonitor
+
+go 1.14
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/shirou/gopsutil v3.21.1+incompatible
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
+)
+
+// SysInfo saves the basic system information
+type SysInfo struct {
+	Hostname string `bson:hostname`
+	Platform string `bson:platform`
+	CPU      string `bson:cpu`
+	RAM      uint64 `bson:ram`
+	Disk     uint64 `bson:disk`
+}
+
+func main() {
+	hostStat, _ := host.Info()
+	cpuStat, _ := cpu.Info()
+	vmStat, _ := mem.VirtualMemory()
+	diskStat, _ := disk.Usage("/") // If you're in Unix change this "\\" for "/"
+
+	info := new(SysInfo)
+
+	info.Hostname = hostStat.Hostname
+	info.Platform = hostStat.Platform
+	info.CPU = cpuStat[0].ModelName
+	info.RAM = vmStat.Total / 1024 / 1024
+	info.Disk = diskStat.Total / 1024 / 1024
+
+	fmt.Printf("%+v\n", info)
+
+}
